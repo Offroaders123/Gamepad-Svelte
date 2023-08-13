bf6428286600ac8fabcbb72c82941cc1faa701f6
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-08-13 01:37:00 -0700

    Lib + Configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  build: {
+    target: "esnext"
+  },
+  server: {
+    port: 5500,
+    strictPort: true
+  },
+  preview: {
+    port: 5500,
+    strictPort: true
+  },
   plugins: [svelte()],
 })
